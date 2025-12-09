3e5f98f1add970b79afb2dd94ee74ce997227b72
Jamshed Ashraf <jamshedashraf19@gmail.com>
2025-12-09 21:34:19 +0500

    deploy

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -28,6 +28,7 @@ export const environment = {
       url: oAuthConfig.issuer,
       rootNamespace: 'AbpAccountPublic',
     },
+    
   },
   remoteServiceErrorHandler: {
     showDetailedErrors: false,
